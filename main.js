afee3150ab01ca54ad2e1b501cc823f75f2ac197
joanbaldo <joanbaldo@hotmail.com>
2023-02-21 23:11:48 +0100

    Todo lo que he podido

--- main.js
+++ main.js
@@ -34,16 +34,59 @@ console.log(valorResto);
 // 3.Lógica de programación
 
 //Crea una variable A cuyo valor sea el número 9, después crea una variable B cuyo valor sea un string con el número 9, desarrolla la lógica para que cuando A == B muestre por consola “Son iguales”, ¿qué pasa si en vez de tener A == B, comprobamos que A === B?
+const A = 9
+const B = '9'
+
+if (A === B) {
+    console.log('Son iguales');
+} else  {
+    console.log('Si aparece este mensaje es porque A se ha comparado con B via === y NO SON el mismo tipo de dato');
+}
+ // +++++++++++   
+//NOTA he puesto el else para que automáticamente al cambiar de == a === escribiese respuesta por consola. Podría haber metido 2 IF's separados tb.
+//Ruego no llaméis a la Santa Inquisición por esto... :-(
+//  +++++++++++
 
 //Crea una variable mochila que sea un array de elementos, desarrolla la lógica para que sí mochila tiene más de 10 elementos muestre por consola el mensaje “No puedo cargar con tantas cosas”, sí mochila contiene entre 10 y 2 elementos se muestre por consola “Que bien voy con mi mochila” y si no muestre por consola “Creo q no necesito una mochila”
+const mochila = [1,2,3,4,5,6,7,8,9,10,11]
+console.log(mochila.length)
+if (mochila.length>10) {
+    console.log("No puedo cargar con tantas cosas")    
+} else if (mochila.length <=10 && mochila.length >=2) {
+    console.log("Que bien voy con mi mochila") 
+}else {
+    console.log("Creo q no necesito una mochila")   
+}
 
 //Crear variable contarHasta10 con valor 0 e incrementar su valor con un bucle hasta que se verifique que contarHasta10 === 10
+let contarHasta10 = 0
+for (let contarHasta10 = 0; contarHasta10 !== 10 ; contarHasta10++) {
+    console.log(` "contarHasta10" vale ${contarHasta10}`);
+    }
+
 
 //Crea una variable diaFestivo que sea de tipo boolean. Crea un ternario que si diaFestivo es true devuelve un console.log de ‘Hoy no trabajo’ y  si es false ‘Hoy trabajo’
 
+const diaFestivo = true ? 'Hoy no trabajo ' : 'Hoy trabajo'
+
+console.log(diaFestivo)
+
+
 //Crea una variable arrayBucle tipo array vacía, rellénala con números del 4 al 18 utilizando un bucle
 
+let arrayBucle = []
+for(let i = 4; i <=18; i++){
+    arrayBucle.push(i)
+}
+console.log(arrayBucle)
+console.log(arrayBucle.length)
+
 //Recorre la variable creada anteriormente arrayBucle ,suma todos sus elementos y guárdalos en una variable de nombre resultado
+let resultado = 0;
+for (let i = 0; i < arrayBucle.length; i++) {
+    resultado += arrayBucle[i];
+}
+console.log(resultado)
 
 //Dado el siguiente array, [‘Con’, ‘Sofia’, ‘aprendiendo’, ‘bucles’], recorrelo utilizando el bucle “for of” y muestra por consola todos sus elementos, después recórrelo utilizando el bucle “for in” mostrando también por consola todos sus elementos
 
